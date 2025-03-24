2f6f0a6a2f57267efce77c492f895e0c86c82767
Khushi Muhammad <156831397+khushimuhammadmasroor13@users.noreply.github.com>
2025-03-24 13:01:09 +0500

    linear search.cpp

---
+++ linear search.cpp
@@ -0,0 +1,34 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int arr[5]={2,1,3,4,5};
+//	int loc=-1;
+//	int n;
+//	cout << "Enter a number you want to search : " << endl;
+//	cin >> n;
+//	for(int i=0;i<5;i++){
+//		if(arr[i]==n)
+//		{
+//			loc=i;
+//		}
+//	}
+//	if(loc==-1)
+//	{
+//		cout <<n << "Not found"<< endl;
+//	}
+//	else
+//	{
+//		cout << n << " found at " <<loc <<endl;
+//	}
+//
+//
+//
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//
+//}
